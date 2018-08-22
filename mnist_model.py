7e60ca1dd81a18849c64864844bc50660bee994b
Akado2009 <prosvirov.k@gmail.com>
2018-08-22 09:18:48 -0700

    almost done

--- mnist_model.py
+++ mnist_model.py
@@ -1,178 +1,168 @@
-#  Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-"""This showcases how simple it is to build image classification networks.
-
-It follows description from this TensorFlow tutorial:
-    https://www.tensorflow.org/versions/master/tutorials/mnist/pros/index.html#deep-mnist-for-experts
-"""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import os
-import sys
 import numpy as np
 import tensorflow as tf
+from PIL import Image
 
-# Configure model options
-TF_DATA_DIR = os.getenv("TF_DATA_DIR", "/tmp/data/")
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+TF_DATA_DIR = os.getenv("TF_DATA_DIR", "/tmp/mnist/")
 TF_MODEL_DIR = os.getenv("TF_MODEL_DIR", None)
 TF_EXPORT_DIR = os.getenv("TF_EXPORT_DIR", "mnist/")
-TF_MODEL_TYPE = os.getenv("TF_MODEL_TYPE", "CNN")
-TF_TRAIN_STEPS = int(os.getenv("TF_TRAIN_STEPS", 200))
+TF_MODEL_TYPE = os.getenv("TF_MODEL_TYPE", "GAN")
+TF_TRAIN_STEPS = int(os.getenv("TF_TRAIN_STEPS", 300))
 TF_BATCH_SIZE = int(os.getenv("TF_BATCH_SIZE", 100))
 TF_LEARNING_RATE = float(os.getenv("TF_LEARNING_RATE", 0.01))
 
-N_DIGITS = 10  # Number of digits.
-X_FEATURE = 'x'  # Name of the input feature.
+
+def model_inputs(real_dim, z_dim):
+    inputs_real = tf.placeholder(tf.float32, (None, real_dim), name="inputs_real")
+    inputs_z = tf.placeholder(tf.float32, (None, z_dim), name="inputs_z")
+
+    return inputs_real, inputs_z
 
 
-def conv_model(features, labels, mode):
-  """2-layer convolution model."""
-  # Reshape feature to 4d tensor with 2nd and 3rd dimensions being
-  # image width and height final dimension being the number of color channels.
-  feature = tf.reshape(features[X_FEATURE], [-1, 28, 28, 1])
+def generator(z, out_dim, n_units=128, reuse=False, alpha=0.01):
+    with tf.variable_scope('generator', reuse=reuse):
+        # Hidden layer
+        h1 = tf.layers.dense(z, n_units, activation=None)
+        # Leaky ReLU
+        h1 = tf.maximum(h1, alpha * h1)
 
-  # First conv layer will compute 32 features for each 5x5 patch
-  with tf.variable_scope('conv_layer1'):
-    h_conv1 = tf.layers.conv2d(
-        feature,
-        filters=32,
-        kernel_size=[5, 5],
-        padding='same',
-        activation=tf.nn.relu)
-    h_pool1 = tf.layers.max_pooling2d(
-        h_conv1, pool_size=2, strides=2, padding='same')
+        # Logits and tanh output
+        logits = tf.layers.dense(h1, out_dim, activation=None)
+        out = tf.nn.tanh(logits)
 
-  # Second conv layer will compute 64 features for each 5x5 patch.
-  with tf.variable_scope('conv_layer2'):
-    h_conv2 = tf.layers.conv2d(
-        h_pool1,
-        filters=64,
-        kernel_size=[5, 5],
-        padding='same',
-        activation=tf.nn.relu)
-    h_pool2 = tf.layers.max_pooling2d(
-        h_conv2, pool_size=2, strides=2, padding='same')
-    # reshape tensor into a batch of vectors
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-
-  # Densely connected layer with 1024 neurons.
-  h_fc1 = tf.layers.dense(h_pool2_flat, 1024, activation=tf.nn.relu)
-  h_fc1 = tf.layers.dropout(
-      h_fc1,
-      rate=0.5,
-      training=(mode == tf.estimator.ModeKeys.TRAIN))
-
-  # Compute logits (1 per class) and compute loss.
-  logits = tf.layers.dense(h_fc1, N_DIGITS, activation=None)
-  predict = tf.nn.softmax(logits)
-  classes = tf.cast(tf.argmax(predict, 1), tf.uint8)
-
-  # Compute predictions.
-  predicted_classes = tf.argmax(logits, 1)
-  if mode == tf.estimator.ModeKeys.PREDICT:
-    predictions = {
-        'class': predicted_classes,
-        'prob': tf.nn.softmax(logits)
-    }
-    return tf.estimator.EstimatorSpec(mode, predictions=predictions,
-        export_outputs={'classes':
-                        tf.estimator.export.PredictOutput({"predictions": predict,
-                                                           "classes": classes})})
-
-  # Compute loss.
-  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
-  # Create training op.
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    optimizer = tf.train.GradientDescentOptimizer(
-        learning_rate=TF_LEARNING_RATE)
-    train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
-    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
-
-  # Compute evaluation metrics.
-  eval_metric_ops = {
-      'accuracy': tf.metrics.accuracy(
-          labels=labels, predictions=predicted_classes)
-  }
-  return tf.estimator.EstimatorSpec(
-      mode, loss=loss, eval_metric_ops=eval_metric_ops)
+        return out, logits
 
 
-def cnn_serving_input_receiver_fn():
-  inputs = {X_FEATURE: tf.placeholder(tf.float32, [None, 28, 28])}
-  return tf.estimator.export.ServingInputReceiver(inputs, inputs)
+def discriminator(x, n_units=128, reuse=False, alpha=0.01):
+    with tf.variable_scope('discriminator', reuse=reuse):
+        # Hidden layer
+        h1 = tf.layers.dense(x, n_units, activation=None)
+        # Leaky ReLU
+        h1 = tf.maximum(h1, alpha * h1)
+
+        logits = tf.layers.dense(h1, 1, activation=None)
+        out = tf.nn.sigmoid(logits)
+
+        return out, logits
 
 
-def linear_serving_input_receiver_fn():
-  inputs = {X_FEATURE: tf.placeholder(tf.float32, (784,))}
-  return tf.estimator.export.ServingInputReceiver(inputs, inputs)
+def main(unused_arguments):
+    mnist = input_data.read_data_sets(TF_DATA_DIR, one_hot=True)
+    input_size = 28 * 28
+    z_size = 100
+    g_hidden_size = 128
+    d_hidden_size = 128
+    alpha = 0.01
+    smooth = 0.1
+    tf.reset_default_graph()
+    input_real, input_z = model_inputs(input_size, z_size)
+
+    g_model, g_logits = generator(input_z, input_size, g_hidden_size, reuse=False, alpha=alpha)
+    d_model_real, d_logits_real = discriminator(input_real, d_hidden_size, reuse=False, alpha=alpha)
+    d_model_fake, d_logits_fake = discriminator(g_model, d_hidden_size, reuse=True, alpha=alpha)
+
+    d_labels_real = tf.ones_like(d_logits_real) * (1 - smooth)
+    d_labels_fake = tf.zeros_like(d_logits_fake)
+
+    d_loss_real = tf.nn.sigmoid_cross_entropy_with_logits(labels=d_labels_real, logits=d_logits_real)
+    d_loss_fake = tf.nn.sigmoid_cross_entropy_with_logits(labels=d_labels_fake, logits=d_logits_fake)
+
+    d_loss = tf.reduce_mean(d_loss_real + d_loss_fake)
+
+    g_loss = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(
+            labels=tf.ones_like(d_logits_fake),
+            logits=d_logits_fake))
 
 
-def main(unused_args): # pylint: disable=unused-argument
-  tf.logging.set_verbosity(tf.logging.INFO)
+    t_vars = tf.trainable_variables()
+    g_vars = [var for var in t_vars if var.name.startswith("generator")]
+    d_vars = [var for var in t_vars if var.name.startswith("discriminator")]
 
-  # Download and load MNIST dataset.
-  mnist = tf.estimator.datasets.DATASETS['mnist'](TF_DATA_DIR)
-  train_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={X_FEATURE: mnist.train.images},
-      y=mnist.train.labels.astype(np.int32),
-      batch_size=TF_BATCH_SIZE,
-      num_epochs=None,
-      shuffle=True)
-  test_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={X_FEATURE: mnist.train.images},
-      y=mnist.train.labels.astype(np.int32),
-      num_epochs=1,
-      shuffle=False)
+    d_train_opt = tf.train.AdamOptimizer().minimize(d_loss, var_list=d_vars)
+    g_train_opt = tf.train.AdamOptimizer().minimize(g_loss, var_list=g_vars)
 
-  if TF_MODEL_TYPE == "LINEAR":
-    # Linear classifier.
-    feature_columns = [
-        tf.feature_column.numeric_column(
-            X_FEATURE, shape=mnist.train.images.shape[1:])]
+    batch_size = TF_BATCH_SIZE
+    epochs = TF_TRAIN_STEPS
+    samples = []
+    losses = []
 
-    classifier = tf.estimator.LinearClassifier(
-        feature_columns=feature_columns, n_classes=N_DIGITS, model_dir=TF_MODEL_DIR)
-    classifier.train(input_fn=train_input_fn, steps=TF_TRAIN_STEPS)
-    scores = classifier.evaluate(input_fn=test_input_fn)
-    print('Accuracy (LinearClassifier): {0:f}'.format(scores['accuracy']))
-    # FIXME This doesn't seem to work. sticking to CNN for the example for now.
-    classifier.export_savedmodel(
-        TF_EXPORT_DIR, linear_serving_input_receiver_fn)
-  elif TF_MODEL_TYPE == "CNN":
-    # Convolutional network
-    training_config = tf.estimator.RunConfig(
-        model_dir=TF_MODEL_DIR, save_summary_steps=100, save_checkpoints_steps=1000)
-    classifier = tf.estimator.Estimator(
-        model_fn=conv_model, model_dir=TF_MODEL_DIR, config=training_config)
-    export_final = tf.estimator.FinalExporter(
-        TF_EXPORT_DIR, serving_input_receiver_fn=cnn_serving_input_receiver_fn)
-    train_spec = tf.estimator.TrainSpec(
-        input_fn=train_input_fn, max_steps=TF_TRAIN_STEPS)
-    eval_spec = tf.estimator.EvalSpec(input_fn=test_input_fn,
-                                      steps=1,
-                                      exporters=export_final,
-                                      throttle_secs=1,
-                                      start_delay_secs=1)
-    tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
-  else:
-    print("No such model type: %s" % TF_MODEL_TYPE)
-    sys.exit(1)
+    export_path = os.path.join(
+        tf.compat.as_bytes(TF_EXPORT_DIR),
+        tf.compat.as_bytes(str(os.getpid())))
 
+    builder = tf.saved_model.builder.SavedModelBuilder(export_path)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for e in range(epochs):
+            for ii in range(mnist.train.num_examples // batch_size):
+                batch = mnist.train.next_batch(batch_size)
+
+                # Get images, reshape and rescale to pass to D
+                batch_images = batch[0].reshape((batch_size, 784))
+                batch_images = batch_images * 2 - 1
+
+                # Sample random noise for G
+                batch_z = np.random.uniform(-1, 1, size=(batch_size, z_size))
+
+                # Run optimizers
+                _ = sess.run(d_train_opt, feed_dict={input_real: batch_images, input_z: batch_z})
+                _ = sess.run(g_train_opt, feed_dict={input_z: batch_z})
+
+            # At the end of each epoch, get the losses and print them out
+            train_loss_d = sess.run(d_loss, {input_z: batch_z, input_real: batch_images})
+            train_loss_g = g_loss.eval({input_z: batch_z})
+
+            print("Epoch {}/{}...".format(e + 1, epochs),
+                  "Discriminator Loss: {:.4f}...".format(train_loss_d),
+                  "Generator Loss: {:.4f}".format(train_loss_g))
+            # Save losses to view after training
+            losses.append((train_loss_d, train_loss_g))
+
+            # Sample from generator as we're training for viewing afterwards
+            sample_z = np.random.uniform(-1, 1, size=(16, z_size))
+            gen_samples = sess.run(
+                generator(input_z, input_size, reuse=True),
+                feed_dict={input_z: sample_z})
+            samples.append(gen_samples)
+
+        test_vector = tf.placeholder(tf.float32, (None, z_size))
+        tensor_info_zvector = tf.saved_model.utils.build_tensor_info(test_vector)
+        #
+        #         # gen_samples = sess.run(
+        #         #     generator(test_vector, input_size, reuse=True),
+        #         #     feed_dict={test_vector: test_vector.eval()}
+        #         # )
+        #
+        sample = generator(test_vector, input_size, reuse=True)
+        #         # extract the segmentation mask
+        #         # output tensor info
+        tensor_info_output = tf.saved_model.utils.build_tensor_info(tf.convert_to_tensor(sample[0]))
+        prediction_signature = (
+            tf.saved_model.signature_def_utils.build_signature_def(
+                inputs={'zVector': tensor_info_zvector},
+                outputs={'image': tensor_info_output},
+                method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME))
+
+        builder.add_meta_graph_and_variables(
+            sess, [tf.saved_model.tag_constants.SERVING],
+            signature_def_map={
+                'generated':
+                    prediction_signature,
+            })
+
+        # export the model
+        builder.save()
 
 if __name__ == '__main__':
   tf.app.run()
